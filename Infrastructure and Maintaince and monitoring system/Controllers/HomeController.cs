c583b972ddef0f97072ddbafed29c5f97adf1557
Naishal <naishal036@gmail.com>
2024-02-12 01:03:12 +0530

    new Files

---
+++ Infrastructure and Maintaince and monitoring system/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Infrastructure_and_Maintaince_and_monitoring_system.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
