3cd17ec54ce4e2927f6e1ad82575fc4fc4faf84c
Naishal Doshi <naishal036@gmail.com>
2024-03-11 01:37:52 +0530

    New Changes

--- Infrastructure and Maintaince and monitoring system/Controllers/HomeController.cs
+++ Infrastructure and Maintaince and monitoring system/Controllers/HomeController.cs
@@ -12,27 +12,102 @@ namespace Infrastructure_and_Maintaince_and_monitoring_system.Controllers
 {
     public class HomeController : Controller
     {
+
+        EmailSending es = new EmailSending();
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        String otp;
         [HandleError]
+
+        public void SendMail(String To)
+        {
+            es.Email = "dashtaxigg@gmail.com";
+            es.To = To;
+            using (MailMessage mm = new MailMessage(es.Email, es.To))
+            {
+                string randomNum = GenerateRandomNumber(6);
+                es.Body = "Your Otp is " + randomNum;
+                otp = randomNum;
+                es.Password = "cgqgsvvqvjwswyyq";
+
+                es.Subject = "Your One Time Password";
+                mm.Subject = es.Subject;
+                mm.Body = es.Body;
+                mm.IsBodyHtml = false;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential cred = new NetworkCredential(es.Email, es.Password);
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = cred;
+                    smtp.Port = 587;
+                    smtp.Send(mm);
+                   
+                }
+            }
+
+
+        }
+        void ConnectionString()
+        {
+            con.ConnectionString = "data source=ASUSTUFGAMING\\SQLEXPRESS; database=IMMS; integrated security=SSPI";
+        }
+        [HttpPost]
         public ActionResult VerifyUser(GetData gs)
         {
-            return View("Naishal");
+            ConnectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select Email from Tbl_Users where LoginID='" + gs.LoginID + "'";
+            dr = com.ExecuteReader();
+            if(!dr.HasRows)
+            {
+                return View("ChangePass",model:"Couldn't find your id");
+            }
+            
+                if (dr.Read())
+                {
+
+
+                    SendMail(dr[0].ToString());
+                    con.Close();
+                    
+                }
+                return View("VerifyOtp");
+            
+            
         }
         public ActionResult Register()
         {
             return View();
         }
-        public ActionResult Login()
+       
+
+        public ActionResult Index()
         {
             return View();
         }
-    
-        public ActionResult Index()
+        public ActionResult VerifyOtp()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Login(String GG)
+        public ActionResult VerifyOtp(int otp)
+        {
+            if (Convert.ToInt32(this.otp)==otp)
+            {
+                return View("Login");
+            }
+            else
+            {
+                return View(model:"Coundn't Verify Otp");
+            }
+        }
+        public ActionResult Login(GetData gd)
         {
             //if (IsValidUser(username, password))
             //{
@@ -43,6 +118,7 @@ namespace Infrastructure_and_Maintaince_and_monitoring_system.Controllers
             //{
             //    ModelState.AddModelError("", "Invalid username or password.");
             //    return View();
+            Session["LoginID"] = gd.LoginID;
             return View();
             //}
         }
@@ -50,7 +126,18 @@ namespace Infrastructure_and_Maintaince_and_monitoring_system.Controllers
         
         public ActionResult ChangePass()
         {
+            if(Session["LoginID"]==null|| Session["LoginID"] == "")
+            {
+                return RedirectToAction("Login");
+                
+            }
+            return View("");
+        }
+        [HttpPost]
+        public ActionResult ChangePass(String newPassword,String oldPassword)
+        {
             return View();
+
         }
 
         private bool IsValidUser(string username, string password)
@@ -68,6 +155,15 @@ namespace Infrastructure_and_Maintaince_and_monitoring_system.Controllers
             //}
             return
                  true;
+        }
+        
+
+        public string GenerateRandomNumber(int numberOfDigits)
+        {
+            Random rand = new Random();
+            int minValue = (int)Math.Pow(10, numberOfDigits - 1);
+            int maxValue = (int)Math.Pow(10, numberOfDigits) - 1;
+            return rand.Next(minValue, maxValue).ToString();
         }
     }
 
---
+++ Infrastructure and Maintaince and monitoring system/Models/EmailSending.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Infrastructure_and_Maintaince_and_monitoring_system.Models
+{
+    public class EmailSending
+    {
+        public String To { get; set; }
+        public String Body { get; set; }
+     
+        public String Email { get; set; }
+        public String Subject { get; set; }
+        public String Password { get; set; }
+    }
+}
