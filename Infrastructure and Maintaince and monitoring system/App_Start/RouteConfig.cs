b9c78cb51d441d1b351fc0b1bfb9967cead763ba
Naishal Doshi <naishal036@gmail.com>
2024-03-10 18:31:45 +0530

    New Update to registration page

--- Infrastructure and Maintaince and monitoring system/App_Start/RouteConfig.cs
+++ Infrastructure and Maintaince and monitoring system/App_Start/RouteConfig.cs
@@ -12,12 +12,25 @@ namespace Infrastructure_and_Maintaince_and_monitoring_system
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+
+            routes.IgnoreRoute("Content/{*pathInfo}");
+
+            routes.IgnoreRoute("Scripts/{*pathInfo}");
+
+            routes.IgnoreRoute("{WebPage}.aspx/{*pathInfo}");
+
+            routes.IgnoreRoute("{resource}.ashx/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+           
+
         }
     }
 }
