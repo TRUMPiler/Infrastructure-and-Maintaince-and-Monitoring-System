aa654bccdc7b25bf81b217760ff7937b05911547
Naishal Doshi <naishal036@gmail.com>
2024-03-16 10:38:54 +0530

    removed unused methods

--- Infrastructure and Maintaince and monitoring system/Controllers/HomeController.cs
+++ Infrastructure and Maintaince and monitoring system/Controllers/HomeController.cs
@@ -135,22 +135,7 @@ namespace Infrastructure_and_Maintaince_and_monitoring_system.Controllers
             }
         }
      
-        private bool IsValidUser(string username, string password)
-        {
-            //using (var con = new SqlConnection(connectionString))
-            //{
-            //    con.Open();
-            //    using (var cmd = new SqlCommand("SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password", con))
-            //    {           
-            //        cmd.Parameters.AddWithValue("@Username", username);
-            //        cmd.Parameters.AddWithValue("@Password", password);
-            //        int count = (int)cmd.ExecuteScalar();
-            //        return count > 0;
-            //    }
-            //}
-            return
-                 true;
-        }
+       
         public ActionResult ForgetPass()
         {
             if (Session["LoginID"] != null)
@@ -194,7 +179,6 @@ namespace Infrastructure_and_Maintaince_and_monitoring_system.Controllers
         {
             if (Session["LoginID"] != null)
             {
-                Session["LoginID"] = null;
                 return RedirectToAction("Index");
             }
             return View();
