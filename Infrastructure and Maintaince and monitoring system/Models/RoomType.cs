ec3c7b5bd0619db0938d691fb8d5e5e227e0c461
Naishal Doshi <naishal036@gmail.com>
2024-04-11 18:28:04 +0530

    Pushing Update for RoomType Model

---
+++ Infrastructure and Maintaince and monitoring system/Models/RoomType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Infrastructure_and_Maintaince_and_monitoring_system.Models
+{
+    public class RoomType
+    {
+    }
+}
